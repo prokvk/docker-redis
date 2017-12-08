671b8acd8a6d2ac8121712047c28e80ca0d8f3e2
prokvk <weirdo@prokvk.com>
2017-12-08 17:21:24 +0100

    updates

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ PWD=$(dirname $SCRIPT)
 exec /usr/bin/docker run --rm --name redis -p 6379:6379 \
 	-v $PWD/config:/etc/redis/6379 \
 	-v $PWD/data:/var/redis/6379 \
-	redis
+	redis redis-server /etc/redis/6379/redis-full.conf
